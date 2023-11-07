de0bfd9188c4416cbc7052c495d2c96ce0ca3cac
Mula Siva Prasad Babu <111627965+Siva-Mula-03@users.noreply.github.com>
2023-11-07 10:08:14 +0530

    Update Insertion Sort.cpp

--- Insertion Sort.cpp
+++ Insertion Sort.cpp
@@ -1,6 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+/*
+void insertion_sort(vector<int>&arr)
+{
+    int n=arr.size();
+    for(int i=1;i<n;i++)
+    {
+        int temp=arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>temp)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        
+        arr[j+1]=temp;
+    }
+}*/
  void insertion_sort(int arr[],int n)
  {
      
